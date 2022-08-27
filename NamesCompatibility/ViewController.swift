7e9c33b0f13c44de887545cb7c067aff5b3e09d6
midniqhtman <c1esi@mail.ru>
2022-08-27 21:33:29 +0300

    Initial Commit

---
+++ NamesCompatibility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesCompatibility
+//
+//  Created by Байсаев Зубайр on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
